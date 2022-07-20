e61fa43be6f758a7875330de7972c831431c5152
Carlos <carlosaltirado@gmail.com>
2022-07-20 09:27:13 -0500

    madebuttonscompletelywork

--- src/App.js
+++ src/App.js
@@ -6,30 +6,26 @@ function App() {
   const [currentSlide, setCurrentSlide] = useState(slides[0])
   const [currentSlideIndex, setCurrentSlideIndex] = useState(0)
 
-  // const n = () => {
-  //   if (currentSlide !== slides.length){
-  //       setCurrentSlide(currentSlide +1 )}
-  // }
-  //   if (currentSlide === slides.length){
-  //       setCurrentSlide(1)
-  //   }
-
-
-
   useEffect(()=>{
     setCurrentSlide(slides[currentSlideIndex])
     },[slides, currentSlideIndex])
 
   return(
-    
     <div className="Slidesapp">
     <h1><strong>Animation Master Class</strong></h1>
     {/* <div> {slides.map((slide, idx) => (<SlidesComponent slide={slide} key={idx}/>))}</div> */}
     <SlidesComponent currentSlide={currentSlide}/>
         <p>
-                <button onClick={() => {setCurrentSlideIndex(0)}}>Sart Over</button>
-                <button onClick={() => {setCurrentSlideIndex(currentSlideIndex - 1)}}>Back</button>
-                <button onClick={() => {setCurrentSlideIndex(currentSlideIndex + 1)}}>Next</button>
+          
+                (<button onClick={() => {setCurrentSlideIndex(0)}}>Sart Over</button>)
+                { currentSlideIndex !== 0 ?
+                (<button onClick={() => {setCurrentSlideIndex(currentSlideIndex - 1)}}>Back</button>):
+                (<button>Back</button>)}
+                
+                { currentSlideIndex < slides.length -1 ?
+                (<button onClick={() => {setCurrentSlideIndex(currentSlideIndex + 1)}}>Next</button>):
+                (<button>Next</button>)}
+          
         </p>
     </div> 
   )
@@ -40,6 +36,7 @@ function SlidesComponent({currentSlide}) {
   return (
     <>
     <div className="eachSlide">
+      
         <div className="img"><img alt="" img src={currentSlide.image} /> </div>  
         <div className='title'><strong>{currentSlide.title}</strong></div>
         <div className='original_title'><strong>{currentSlide.original_title}</strong></div>  
